e710b365c36ead7784270cb175e36d90a2ba73fb
PaulRwagasana <p.rwagasana@alustudent.com>
2025-11-22 14:38:43 +0200

    added new screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'screens/homepage_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
-import 'theme/app_theme.dart';
+import 'screens/jobs_screen.dart';
+import 'screens/volunteer_screen.dart';
+import 'screens/explore_screen.dart';
+import 'screens/profile_screen.dart';
 
 void main() {
   runApp(const ZambiConnectApp());
@@ -15,13 +18,30 @@ class ZambiConnectApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ZambiConnect',
-      theme: AppTheme.lightTheme,
+      theme: ThemeData(
+        primaryColor: Color(0xFF2E7D32),
+        scaffoldBackgroundColor: Color(0xFFF5F5F5),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Color(0xFF333333)),
+          titleTextStyle: TextStyle(
+            color: Color(0xFF333333),
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginScreen(),
         '/signup': (context) => const SignupScreen(),
         '/homepage': (context) => const HomepageScreen(),
+        '/jobs': (context) => const JobsScreen(),
+        '/volunteer': (context) => const VolunteerScreen(),
+        '/explore': (context) => const ExploreScreen(),
+        '/profile': (context) => const ProfileScreen(),
       },
     );
   }
